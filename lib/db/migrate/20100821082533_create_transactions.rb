a02cf2dec00d9e6224905c9036802cf1985a9775
Michael He <hlxwell@gmail.com>
2010-08-21 16:41:44 +0800

    add migration

--- lib/db/migrate/20100821082533_create_transactions.rb
+++ lib/db/migrate/20100821082533_create_transactions.rb
@@ -1,7 +1,26 @@
 class CreateTransactions < ActiveRecord::Migration
   def self.up
+    create_table :transactions do |t|
+      t.string      "type"
+      t.references  "parent", :polymorphic => true
+      t.references  "related_object", :polymorphic => true
+      t.string      "credit_type"
+      t.string      "credit_from"
+      t.string      "credit_to"
+      t.integer     "amount"
+      t.string      "state"
+      t.string      "note"
+      t.datetime    "done_at"
+      t.datetime    "deleted_at"
+      t.timestamps
+    end
+
+    add_index :transactions, :credit_type
+    add_index :transactions, [:parent_type, :parent_id]
+    add_index :transactions, [:related_object_type, :related_object_id]
   end
 
   def self.down
+    drop_table :transactions
   end
 end
--- lib/generators/bank_center/bank_center_generator.rb
+++ lib/generators/bank_center/bank_center_generator.rb
@@ -2,7 +2,7 @@ class BankCenterGenerator < Rails::Generator::Base
   def manifest
     record do |m|
       m.file "test.txt", "test.txt"
-      # m.migration_template 'migration.rb', 'db/migrate', :migration_file_name => "acts_as_taggable_on_migration"
+      # m.migration_template 'migration.rb', 'db/migrate', :migration_file_name => "bank_center_on_migration"
     end
   end
 end
